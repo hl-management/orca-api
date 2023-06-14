7006fea68a4d249355e94aefba355082e0ee983d
RUIJIE.HAO <datouhaoruijie@gmail.com>
2023-06-15 08:47:44 +0900

    finish

---
+++ lib/orca_api/master_export_service.rb
@@ -0,0 +1,27 @@
+require_relative "service"
+
+module OrcaApi
+  class MasterExportService < Service
+    
+    # マスタ取得実行リクエスト（Request_Number=01）
+    #
+    # @see https://www.orcamo.co.jp/api-council/members/standards/?haori_master_export
+    def export(params)
+      call("01", params)
+    end
+
+    def result(params)
+      call("02", params)
+    end
+
+    private
+
+    def call(request_number, params)
+      params[:Request_Number] = request_number
+
+      Result.new(orca_api.call("/orca51/masterexportv3", body: {
+        "master_exportv3req" => params
+      }))
+    end
+  end
+end
