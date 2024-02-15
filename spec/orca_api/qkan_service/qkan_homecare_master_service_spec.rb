1d58493dd164cebfc3584cc8556548e9a92c45a4
khoanda <khoanda@hopee.com.vn>
2024-02-15 14:09:34 +0700

    Rspec

---
+++ spec/orca_api/qkan_service/qkan_homecare_master_service_spec.rb
@@ -0,0 +1,26 @@
+# spec/orca_api/qkan_service/qkan_provider_list_service_spec.rb
+require "spec_helper"
+require "orca_api/orca_qkan_service/qkan_homecare_master_service"
+
+RSpec.describe OrcaApi::OrcaQkanService::QkanHomecareMasterService do # rubocop:disable RSpec/SpecFilePathFormat, RSpec/FilePath
+  orca_model = 'OrcaApi::Api'
+  let(:orca_api) { instance_double(orca_model, call: api_response) }
+  let(:service) { described_class.new(orca_api) }
+
+  describe "#get" do
+    let(:api_response) do
+      {
+        success: true,
+        response: load_orca_api_response("mst01_mservice_codelst.json")
+        # Add other necessary keys based on the actual response structure
+      }
+    end
+
+    it "returns the API response" do
+      result = service.get
+
+      expect(result[:success]).to be true
+      expect(result[:response]).to include("処理終了")
+    end
+  end
+end
