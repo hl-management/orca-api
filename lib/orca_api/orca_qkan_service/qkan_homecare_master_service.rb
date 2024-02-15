9dd1680832e14f1e5d7068347c54db74f768dc70
khoanda <khoanda@hopee.com.vn>
2024-02-15 12:53:58 +0700

    Implement PBI -1504 -v4

--- lib/orca_api/orca_qkan_service/qkan_homecare_master_service.rb
+++ lib/orca_api/orca_qkan_service/qkan_homecare_master_service.rb
@@ -9,8 +9,25 @@ module OrcaApi
           body: <<-XML
               <data>
                 <mservicecodelstreq type="record">
-                  <System_Service_Kind_Detail type="string">11111</System_Service_Kind_Detail>
+                  <Service_Code_Kind type="string">13</System_Service_Kind_Detail>
                   <Target_Date type="string">#{target_date}</Target_Date>
+                  <Kasan_Flag type="string">0</Kasan_Flag>
+                  <Santei_Item_Information type="array">
+                    <!-- 算定項目情報(施設等の区分) -->
+                    <Santei_Item_Information_child type="record">
+                        <!-- 10-1_算定項目番号 -->
+                        <Item_No type="string">1</Item_No>
+                        <!-- 10-2_算定項目検索値 -->
+                        <Search_Value type="string">1</Search_Value>
+                    </Santei_Item_Information_child>
+                    <!-- 算定項目情報(職員区分) -->
+                    <Santei_Item_Information_child type="record">
+                        <!-- 10-1_算定項目番号 -->
+                        <Item_No type="string">3</Item_No>
+                        <!-- 10-2_算定項目検索値 -->
+                        <Search_Value type="string">1</Search_Value>
+                    </Santei_Item_Information_child>
+                  </Santei_Item_Information>
                 </mservicecodelstreq>
               </data>
           XML
