68d72f7be24b323afadd0556da182a835e97c2c0
khoanda <khoanda@hopee.com.vn>
2024-05-30 10:57:57 +0700

    [hotfix-1506]: break API get list and get details

--- lib/orca_api/orca_qkan_service/patient_payget_service.rb
+++ lib/orca_api/orca_qkan_service/patient_payget_service.rb
@@ -18,6 +18,22 @@ module OrcaApi
           XML
         )
       end
+
+      def list(provider_id, date, result_type = 'pdf')
+        orca_api.call(
+          "/claim01/patientpayget",
+          format: 'xml',
+          body: <<-XML
+              <data>
+                <patientpaygetreq type="record">
+                  <Claim_Date type="string">#{date}</Claim_Date>
+                  <Result_Type type="string">#{result_type.upcase}</Result_Type>
+                  <Provider_Id type="string">#{provider_id}</Provider_Id>
+                </patientpaygetreq>
+              </data>
+          XML
+        )
+      end
     end
   end
 end
